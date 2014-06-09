c69b00faebd1fd4088a30cc0f11d1d34c2ef2602
hamdouni <brahim@hamdouni.com>
2014-06-09 18:59:22 +0200

    Adding database access

--- serv.go
+++ serv.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"flag"
 	"fmt"
 	"net/http"
@@ -10,6 +11,8 @@ import (
 	"mime"
 	"path/filepath"
 	"strconv"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql" 
 )
 
 var port int
@@ -51,8 +54,40 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", body)
 }
 
+func serviceHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
+	print ("Connecting to database? ")
+	db, err := sql.Open("mysql","root:@/prod")
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	println ("OK")
+
+	var (
+		pv_id int
+		pv_nom string
+	)
+	rows, err := db.Query("select pv_id,pv_nom from pdv limit 10")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&pv_id, &pv_nom)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(pv_id, pv_nom)
+	}
+
 	println ("Server start on port ", port)
 	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/service", serviceHandler)
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
