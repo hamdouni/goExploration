5d3fc4ea8441346e6c63d6bd4c5d0327dd0a71fc
hamdouni <brahim@hamdouni.com>
2017-11-04 00:01:06 +0100

    more go idiomatic

--- input/main.go
+++ input/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -11,21 +10,22 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	out := false
 	rand.Seed(time.Now().UnixNano())
-	guess := rand.Intn(100)
+	guess := rand.Intn(10)
 	count := 0
-	println("Guess a number between 1 and 100. q to quit.")
-	for !out {
+	println("Guess a number between 1 and 10. q to quit. ==> ", guess)
+loop:
+	for {
 		count++
 		scanner.Scan()
 		t := scanner.Text()
 		if t == "q" {
-			out = true
+			break
 		}
 		r, e := strconv.Atoi(t)
 		if e != nil {
 			println("not a number - try again")
+			continue
 		}
 		switch {
 		case r < guess:
@@ -33,8 +33,8 @@ func main() {
 		case r > guess:
 			println("too much")
 		default:
-			fmt.Printf("you win after %v attempts.\n", count)
-			out = true
+			println("you win after", count, "attempts.")
+			break loop
 		}
 	}
 }
