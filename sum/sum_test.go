554d08e09169398d6f42694a174e057c53947b64
Brahim Hamdouni <brahim@hamdouni.com>
2019-05-03 12:42:23 +0200

    Add sum of only one number test case

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -10,6 +10,7 @@ func TestSum(t *testing.T) {
 		lab string
 	}{
 		{[]int{}, 0, "sum of none = 0 "},
+		{[]int{9}, 9, "sum of 9 = 9 "},
 		{[]int{1, 2, 3, 4, 5, 6}, 21, "sum 1 to 6 = 21"},
 		{[]int{1, -1}, 0, "sum 1 -1 = 0"},
 		{[]int{4, 5, 6}, 15, "sum 4 to 6 = 15"},
