65eb788e121387ebb75a30130b981132dbdf11fe
Brahim Hamdouni <brahim@hamdouni.com>
2022-11-02 00:05:02 +0100

    p5go: more animation

--- p5go/main.go
+++ p5go/main.go
@@ -6,30 +6,29 @@ import (
 	"github.com/go-p5/p5"
 )
 
-var a float64
+var a, posx, posy float64
 
 func main() {
-	var x int
-
 	p5.Run(setup, draw)
-
-	_ = x
 }
 
 func setup() {
 	p5.Canvas(400, 200)
-	p5.Background(color.Gray{Y: 220})
 }
 
 func draw() {
-
-	p5.Fill(color.RGBA{R: 255, G: 108, B: 105, A: 255})
+	p5.Fill(color.Gray{Y: 120})
 	p5.Rect(0, 0, 400, 200)
-
-	p5.Translate(200, 100)
+	p5.Translate(200,100)
 	a = a + 0.01
 	p5.Rotate(a)
+	p5.Fill(color.RGBA{R: 255, G: 108, B: 105, A: 255})
 	p5.Rect(30, 10, 38, 58)
 	p5.Triangle(-30, -20, -20, 30, 40, -10)
 	p5.Circle(0, 0, 10)
+	posx++
+	posy++
+	if posx > 400 && posy > 200 {
+		posx, posy = 0, 0
+	}
 }
