7975c343c1d19aa4d50c73763f2176d4779e25fa
Brahim Hamdouni <brahim@hamdouni.com>
2024-11-11 19:22:36 +0100

    Idiomatic Go

--- argumentsflags/argumentsflags.go
+++ argumentsflags/argumentsflags.go
@@ -6,16 +6,22 @@ import (
 	"time"
 )
 
+const dateformat = "2006-01-02"
+
 func main() {
-	var arg string
-	flag.StringVar(&arg, "date", "", "format YYYY-MM-DD")
+	var date string
+	flag.StringVar(&date, "date", time.Now().Format(dateformat), "format YYYY-MM-DD")
 	flag.Parse()
-	if arg == "" {
-		panic("argument date required")
+
+	if len(date) == 0 {
+		fmt.Println(time.Now())
+		return
 	}
-	d, e := time.Parse("2006-01-02", arg)
-	if e != nil {
-		panic(e)
+
+	d, err := time.Parse(dateformat, date)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
 	fmt.Println(d)
